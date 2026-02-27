89643a88ec0a238a1c1594a6e2b2cd337af81f19
0-don <don.cryptus@gmail.com>
2026-02-28 00:15:09 +0100

    fix: remove avif format from image configurations in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,16 @@ import createNextIntlPlugin from "next-intl/plugin";
 const nextConfig: NextConfig = {
   output: process.env.STANDALONE ? "standalone" : undefined,
   images: {
-    formats: ["image/avif", "image/webp"],
+    formats: ["image/webp"],
     qualities: [10, 25, 50, 75, 90, 100],
-    minimumCacheTTL: 60 * 60 * 24,
-  },
+    minimumCacheTTL: 60 * 60 * 24
+  }
 };
 
 const withNextIntl = createNextIntlPlugin({
   experimental: {
-    createMessagesDeclaration: ["./public/i18n/en.json"],
-  },
+    createMessagesDeclaration: ["./public/i18n/en.json"]
+  }
 });
 
 export default withNextIntl(nextConfig);
