670613d06722d2caa4b09e82a049b230016fc2ae
0-don <don.cryptus@gmail.com>
2026-03-26 22:16:35 +0100

    fix: comment out productionBrowserSourceMaps in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   output: process.env.STANDALONE ? "standalone" : undefined,
-  productionBrowserSourceMaps: true,
+  // productionBrowserSourceMaps: true,
   // experimental: {
   //   allowDevelopmentBuild: true,
   // },
