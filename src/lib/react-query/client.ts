0f559f4f2f80a2ba2b46c171d727d71e046647e8
0-don <don.cryptus@gmail.com>
2026-03-28 22:19:35 +0100

    fix: replace deprecated isServer with environmentManager.isServer for improved compatibility

--- src/lib/react-query/client.ts
+++ src/lib/react-query/client.ts
@@ -1,4 +1,4 @@
-import { QueryClient, isServer } from "@tanstack/react-query";
+import { environmentManager, QueryClient } from "@tanstack/react-query";
 import { cache } from "react";
 
 function makeQueryClient() {
@@ -17,7 +17,7 @@ const getServerQueryClient = cache(makeQueryClient);
 let browserQueryClient: QueryClient | undefined = undefined;
 
 export default function getQueryClient() {
-  if (isServer) {
+  if (environmentManager.isServer()) {
     return getServerQueryClient();
   }
   if (!browserQueryClient) browserQueryClient = makeQueryClient();
