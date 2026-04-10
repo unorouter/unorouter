766fb382dd956f87957b6d1f3422e23593970eba
0-don <don.cryptus@gmail.com>
2026-04-10 02:14:51 +0200

    refactor(referral-capture): remove unused router import and comment out navigation logic

--- src/components/pages/navbar/home/referral-capture.tsx
+++ src/components/pages/navbar/home/referral-capture.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useRouter } from "@/i18n/navigation";
 import { AFF_CODE_KEY, COOKIE_MAX_AGE } from "@/lib/config/constants";
 import { setCookie } from "cookies-next/client";
 import { useSearchParams } from "next/navigation";
@@ -8,15 +7,24 @@ import { useEffect } from "react";
 
 export function ReferralCapture() {
   const searchParams = useSearchParams();
-  const router = useRouter();
 
   useEffect(() => {
     const ref = searchParams.get("ref");
     if (ref) {
       setCookie(AFF_CODE_KEY, ref, { maxAge: COOKIE_MAX_AGE });
-      router.push("/register");
+      // router.push("/register");
+      const params = new URLSearchParams(searchParams.toString());
+      params.delete("ref");
+      const query = params.toString();
+      window.history.replaceState(
+        null,
+        "",
+        query
+          ? `${window.location.pathname}?${query}`
+          : window.location.pathname,
+      );
     }
-  }, [searchParams, router]);
+  }, [searchParams]);
 
   return null;
 }
