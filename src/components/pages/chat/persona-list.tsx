24969e6b0bca7ffdfa86218bae771a9317667d3f
0-don <don.cryptus@gmail.com>
2026-05-05 01:11:18 +0200

    refactor: remove unused useRef import from persona-list and preset-list components

--- src/components/pages/chat/persona-list.tsx
+++ src/components/pages/chat/persona-list.tsx
@@ -20,7 +20,7 @@ import {
   useUpdatePersonaMutation,
 } from "@/hooks/rp-hook";
 import { useTranslations } from "next-intl";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { LuPlus, LuTrash2 } from "react-icons/lu";
 
 type Props = {
