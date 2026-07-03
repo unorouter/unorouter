010bab936460378af447472fd6055ea843c2e86c
0-don <don.cryptus@gmail.com>
2026-07-03 17:03:20 +0200

    chore(footer): revert CodeTrendy badge to self-hosted src

--- src/components/layout/nav/footer.tsx
+++ src/components/layout/nav/footer.tsx
@@ -100,7 +100,7 @@ const RECIPROCAL_LINKS = [
   },
   {
     href: "https://codetrendy.com/?utm_source=unorouter.com&utm_medium=badge",
-    src: "https://codetrendy.com/api/badge?style=classic",
+    src: "/badges/codetrendy.svg",
     name: "CodeTrendy",
     width: 181,
     height: 46,
