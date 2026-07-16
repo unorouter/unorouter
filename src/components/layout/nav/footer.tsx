ceaeaeb5507813122da467b8ffcdb334f1b6d12b
0-don <don.cryptus@gmail.com>
2026-07-16 11:19:36 +0200

    feat(footer): add LaunchBuff badge to footer

--- src/components/layout/nav/footer.tsx
+++ src/components/layout/nav/footer.tsx
@@ -109,6 +109,12 @@ const FOOTER_BADGES = [
     name: "LaunchBoosts",
     width: 153,
   },
+  {
+    href: "https://launchbuff.com",
+    src: "/badges/launchbuff.svg",
+    name: "LaunchBuff",
+    width: 77,
+  },
 ] as const;
 
 function FooterLinks(props: {
