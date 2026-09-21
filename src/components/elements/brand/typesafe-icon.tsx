67cee43dc896a6901241782aebf2d93729833ecc
0-don <don.cryptus@gmail.com>
2026-09-22 00:39:14 +0200

    Vendors: TypeSafe icon and theme

---
+++ src/components/elements/brand/typesafe-icon.tsx
@@ -0,0 +1,8 @@
+import { makeImgIcon } from "@/components/elements/brand/vendor-img-icon";
+
+const TypeSafeIcon = makeImgIcon(
+  "/images/icons/vendors/typesafe.webp",
+  "TypeSafe",
+);
+
+export default TypeSafeIcon;
